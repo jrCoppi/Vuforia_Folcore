cb5d98993492e01624a603b3f1e9eb135f78424f
Junior Coppi <jr.coppi@gmail.com>
2017-05-13 16:35:06 -0300

    Boto + script
    Boto + script

---
+++ Assets/DesativaObjetos.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DesativaObjetos : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GameObject[] allObjects = FindObjectsOfType<GameObject>();
+		foreach (GameObject a in allObjects) {
+			if (a.name == "VitoriaRegia") {
+				a.SetActive (false);
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
