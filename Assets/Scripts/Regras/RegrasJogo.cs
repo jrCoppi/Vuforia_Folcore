54bb23a2d8b380c4b29e3f58aa2aff314c56ec57
william-lpa <william-lpa@hotmail.com>
2017-05-20 13:35:59 -0300

    inclusao arquivo faltante

--- Assets/Scripts/Regras/RegrasJogo.cs
+++ Assets/Scripts/Regras/RegrasJogo.cs
@@ -8,9 +8,12 @@ namespace Assets.Scripts.Regras
 {
     class RegrasJogo : MonoBehaviour, IGameStatus
     {
-        private Status status;
+        private Status status = Status.Nao_Iniciado;
+        private int pontuacaoAtual = 0;
+        public PerguntaJogo PerguntaAtual { get; set; }
 
-        public Status MyProperty
+
+        public Status GameStatus
         {
             get { return status; }
             set
@@ -20,50 +23,93 @@ namespace Assets.Scripts.Regras
             }
         }
 
-        public Status GameStatus
-        {
-            get
-            {
-                throw new NotImplementedException();
-            }
-        }
-
         public int Pontuacao
         {
             get
             {
-                throw new NotImplementedException();
+                return Pontuacao;
             }
         }
 
+
         public void IniciarJogo()
         {
-            throw new NotImplementedException();
+            GameStatus = Status.Em_Andamento;
+            Setup.Instance.InicializarTabuleiro();
+            // transicionar cena(ver como avança em cenas, sair do menu principak pra jogar)
         }
 
-        public void PassarQuestao()
+
+        private void RespostaCorreta(bool errouUmaVez)
         {
-            throw new NotImplementedException();
+            if (!errouUmaVez)
+                pontuacaoAtual += 10;
+            else
+                pontuacaoAtual += 5;
+
         }
 
-        public void ProximaQuestao()
+        private void RespostaIncorreta()
         {
-            throw new NotImplementedException();
-        }
-
-        public void RespostaCorreta(bool errouUmaVez)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void RespostaIncorreta()
-        {
-            throw new NotImplementedException();
+            pontuacaoAtual -= 2;
         }
 
         public void TerminarJogo()
         {
             throw new NotImplementedException();
         }
+        public void PassarQuestao()
+        {
+            RespostaIncorreta();
+            //ProximaQuestao();
+        }
+
+        public void ProximaQuestao(Objeto personagem)
+        {
+            var alternativas = GerarAlternativas(personagem);
+            PerguntaAtual = new PerguntaJogo(alternativas[0].ToString(), alternativas.Select(x => x.Personagem.GetDescription()).ToArray());
+        }
+
+        private Objeto[] GerarAlternativas(Objeto personagem, int numeroAlternativas = 4)
+        {
+            var chaves = new HashSet<Objeto>() { personagem };
+            var maxID = Setup.Instance.ObjetosNoJogo.Max(x => x.Id);
+
+            for (int i = 1; i < numeroAlternativas; i++)
+            {
+                var idAleatorio = new System.Random().Next(maxID);
+                var personagemAleatorio = Setup.Instance.ObjetosNoJogo.First(x => x.Id == idAleatorio);
+
+                if (!chaves.Contains(personagemAleatorio))
+                {
+                    chaves.Add(personagemAleatorio);
+                }
+            }
+            return chaves.ToArray();
+        }
+
+        public Posicao.Desempenho ResponderPergunta(string resposta)
+        {
+            if (resposta == PerguntaAtual.AlternativaCorreta && PerguntaAtual.Tentativas == 0)
+            {
+                RespostaCorreta(false);
+                return Posicao.Desempenho.Acerto;
+            }
+            if (resposta == PerguntaAtual.AlternativaCorreta && PerguntaAtual.Tentativas == 1)
+            {
+                RespostaCorreta(true);
+                return Posicao.Desempenho.Acerto_Com_Erro;
+            }
+            if (resposta != PerguntaAtual.AlternativaCorreta && PerguntaAtual.Tentativas == 1)
+            {
+                RespostaIncorreta();
+                return Posicao.Desempenho.Erro;
+            }
+
+            PerguntaAtual.Tentativas++;
+            return Posicao.Desempenho.Nao_Respondido;
+        }
+
+
     }
 }
