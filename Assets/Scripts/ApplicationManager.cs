a9bbec3f78f53e8847112443ae6fba994f31c301
alanmello16 <alanmello16@gmail.com>
2017-06-19 19:16:30 -0300

    Adicionado menu principal

---
+++ Assets/Scripts/ApplicationManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ApplicationManager : MonoBehaviour {
+
+	public void NextLevelButton(string levelName)
+	{
+		Application.LoadLevel(levelName);
+	}
+	
+
+	public void Quit () 
+	{
+		#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+		#else
+		Application.Quit();
+		#endif
+	}
+}
